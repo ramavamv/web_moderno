499d7fe38244873a99f5e978137d42178e78b077
ramavamv <ramavamv@yahoo.com.br>
2026-07-09 22:30:39 -0300

    #0009 - Seção 3 anotações

---
+++ secao_3/desafio.js
@@ -0,0 +1,29 @@
+
+// for (let i = 1; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// const PI = 3.141592;
+// let raio = 10;
+
+// console.log(raio * raio * PI);
+// console.log(raio * raio * Math.PI);
+
+
+// troca de valores de variáveis, posso usar uma variável temporária ou usar desta forma 
+let a = 7;
+let b = 94;
+console.log(a, b);
+[a, b] = [b, a];
+console.log(a, b);
+
+
+// \n  =  ${os.EOL}
+
+// FIFO = primeiro entrar primeiro sair
+
+// LIFO = último entrar é o primeiro a sair
